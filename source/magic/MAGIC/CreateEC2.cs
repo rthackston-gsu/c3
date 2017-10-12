362fd6ad56c33d670c21d8d49f93004a47ef2820
Mehtab Iqbal (Shahan) <mi00806@GeorgiaSouthern.edu>
2017-10-12 01:16:55 -0400

    Added S3 Bucket creation code

---
+++ source/magic/MAGIC/CreateEC2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Amazon.EC2;
+using Amazon.EC2.model;
+using Amazon.Util;
+
+namespace magic.gsu.edu
+{
+    class CreateEC2
+    {
+        ec2Client = new AmazonEC2Client()
+    }
+}
---
+++ source/magic/MAGIC/CreateS3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Util;
+
+namespace magic.gsu.edu
+{
+    class CreateS3
+    {
+        static string bucketName = "magic.bucket.one";
+        public static void Main(string[] args)
+        {
+            using (var client = new AmazonS3Client(Amazon.RegionEndpoint.USWest2))
+            {
+                if (!(AmazonS3Util.DoesS3BucketExist(client, bucketName)))
+                {
+                    Console.Write("Creating Bucket");
+                    CreateABucket(client);
+                }
+                string bucketLocation = FindBucketLocation(client);
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static string FindBucketLocation(IAmazonS3 client)
+        {
+            GetBucketLocationRequest request = new GetBucketLocationRequest()
+            {
+                BucketName = bucketName
+            };
+            GetBucketLocationResponse response = client.GetBucketLocation(request); 
+            return response.Location.ToString();
+        }
+
+        static void CreateABucket(IAmazonS3 client)
+        {
+            try
+            {
+                PutBucketRequest putRequest = new PutBucketRequest
+                {
+                    BucketName = bucketName,
+                    UseClientRegion = true
+                };
+                PutBucketResponse response = client.PutBucket(putRequest);
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                if (amazonS3Exception.ErrorCode != null &&
+                    amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                    ||
+                    amazonS3Exception.ErrorCode.Equals("InvalidSecurity"))
+                {
+                    Console.WriteLine("Check the provided AWS Credentials.");
+                    Console.WriteLine("For service sign up go to http://aws.amazon.com/s3");
+                }
+                else
+                {
+                    Console.WriteLine("Error occurred. Message: '{0}' when writing an object",
+                        amazonS3Exception.Message);
+                }
+            }
+        }
+    }
+}
