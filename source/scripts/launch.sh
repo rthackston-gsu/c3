3331cb0e3bd05dc8808978c2f77aa378d6a7f2a3
Mehtab Iqbal (Shahan) <mehtab_iqbal@georgiasouthern.edu>
2017-10-26 10:16:33 -0400

    aws configs

--- source/scripts/launch.sh
+++ source/scripts/launch.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-curl -o taskFile.sh https://s3-us-west-2.amazonaws.com/magic-bucket-one/task.sh
+curl -o taskFile.sh https://s3-us-west-2.amazonaws.com/<bucket_guid>/task.sh
 sh taskFile.sh
 apt install awscli -y
+aws configure set aws_access_key_id <access_key_id>
+aws configure set aws_secret_access_key <secret_access_key>
+aws configure set region <region_name>
+aws configure set output json
 echo "aws ec2 run-instances --image-id ami-6e1a0117 --count 1 --instance-type t2.micro \
---key-name shahan_test --subnet-id subnet-f41f8e92 --security-group-ids launch-wizard-2 \
+--key-name <private_key.pem> --security-group-ids launch-wizard-2 \
 --user-data file://taskFile.sh"
