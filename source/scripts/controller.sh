dbde721b8fe9aaf15f3ebc2d536b24f7b3836929
Mehtab Iqbal (Shahan) <mehtab_iqbal@georgiasouthern.edu>
2017-11-02 02:39:19 -0400

    userdata script

--- source/scripts/controller.sh
+++ source/scripts/controller.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-curl -o taskFile.sh https://s3-us-west-2.amazonaws.com/<bucket_guid>/task.sh
-sh taskFile.sh
 apt install awscli -y
 aws configure set aws_access_key_id <access_key_id>
 aws configure set aws_secret_access_key <secret_access_key>
 aws configure set region <region_name>
 aws configure set output json
-echo "aws ec2 run-instances --image-id ami-6e1a0117 --count 1 --instance-type t2.micro \
+(aws ec2 run-instances --image-id ami-6e1a0117 --count 1 --instance-type t2.micro \
 --key-name <private_key.pem> --security-group-ids launch-wizard-2 \
---user-data file://taskFile.sh"
+--user-data file://taskFile.sh)
---
+++ source/scripts/userdata.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+GUID=<GUID>
+echo '$GUID'
+
+YUM_CMD=$(which yum)
+APT_CMD=$(which apt)
+APT_GET_CMD=$(which apt-get)
+
+get_package() {
+  PACKAGE = $1
+  if [[ ! -z $YUM_CMD ]]; then
+    yum install $PACKAGE -y
+  elif [[ ! -z $APT_CMD ]]; then
+    apt install $PACKAGE -y
+  elif [[ ! -z $APT_GET_CMD ]]; then
+    apt-get install $PACKAGE -y
+  else
+    echo "Could not install $PACKAGE due to incompatible package manager"
+    exit 1;
+  fi
+}
+
+get_package awscli
+(aws s3 cp s3://$GUID/controller.sh controller.sh)
