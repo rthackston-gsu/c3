8e004826f67f2409a29d092ba97727ada5c8749a
Beloved Anetor <integralbeloved@yahoo.com>
2017-11-03 20:44:34 -0400

    Beloved app3

---
+++ source/App3/App3/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using App3.Utility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MagicServiceUtility msu = new MagicServiceUtility();
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            //Calls EC2 AWS SDK
+            Task<string> getMagicTask = MagicServiceUtility.GetServiceOutputAsync();
+        
+            string result = await getMagicTask;
+            
+            Debug.WriteLine("Characters received: " +result + "\n"+result.Length);
+            if (result.Length > 0)
+            {
+
+             //   textBlock.FontSize = 25;
+            //    textBlock.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                textBlock.Text = textBlock.Text + "\n" + result;
+            }
+            */
+
+            Task<string> createS3Buccket = MagicServiceUtility.createS3();
+
+
+            string resultBucket = "";
+           
+                resultBucket = await createS3Buccket;
+           
+
+            Debug.WriteLine("Characters received: " + resultBucket + "\n" + resultBucket.Length);
+            if (resultBucket.Length > 0)
+            {
+
+                //   textBlock.FontSize = 25;
+                //    textBlock.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                textBlock.Text = textBlock.Text + "\n" + resultBucket;
+
+
+
+            }
+            /*
+            Task<string> createEc2V = MagicServiceUtility.createEc2();
+            string res = await createEc2V;
+            if (resultBucket.Length > 0)
+            {
+
+                //   textBlock.FontSize = 25;
+                //    textBlock.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                textBlock.Text = "\n" + resultBucket;
+
+
+
+            }
+            */
+        }
+
+        #region Button Click Event Handlers
+        private void browseFile_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            Fil dlg = new Microsoft.Win32.OpenFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                this.UploadFile = dlg.FileName;
+            }
+            */
+
+        }
+        #endregion
+
+    }
+}
---
+++ source/App3/App3/Utility/Constants.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3.Utility
+{
+    class Constants
+    {
+        public static string worker_text_content = "#!/bin/bash \n \n" + "BUCKET=<bucket_name> \n" + "GUID=<bucket_guid> \n" +
+            " echo '$GUID' \n" +
+            "\apt install awscli -y \n"
+            +
+            "aws configure set aws_access_key_id <access_key_id>\n" +
+            "aws configure set aws_secret_access_key <secret_access_key>\n" +
+            "aws configure set region <region_name>\n" +
+            "aws configure set output json \n" +
+            "(aws s3 cp s3://$BUCKET/$GUID/task.sh task.sh) \n" +
+            "(bash task.sh)\n"
+            +
+            "(aws s3 cp result.txt s3://$BUCKET/$GUID/result.txt)\n"
+            +
+            "(shutdown -h now)\n";
+
+        
+        public static string ud_file_text_content = "#!/bin/bash \n touch /tmp/test.txt";
+
+        public static string task_file_text_content = "#!/bin/bash \necho 'hello world' > result.txt";
+
+        public static string controller_text_content = "#!/bin/bash \n \n" + "BUCKET=<bucket_name> \n" + "GUID=<bucket_guid> \n" +
+            " echo '$GUID' \n" +
+            "aws configure set aws_access_key_id <access_key_id>\n" +
+            "aws configure set aws_secret_access_key <secret_access_key>\n" +
+            "aws configure set region <region_name>\n" +
+            "aws configure set output json \n" +
+            "(aws s3 cp s3://$BUCKET/$GUID/worker.sh worker.sh) \n"+
+            "(aws ec2 run-instances --image-id ami-6e1a0117 --count 1 --instance-type t2.micro --user-data file://worker.sh) ";
+
+        public static string config_text_content = "#!bin/bash \n <GUID>";
+
+        public static string user_data_string = "#!/bin/bash \n \n" +"BUCKET=<bucket_name> \n" + "GUID=<bucket_guid> \n" +
+            " echo '$GUID' \n" +
+            " YUM_CMD=$(which yum) \n" +
+            " APT_CMD =$(which apt) \n" +
+            " APT_GET_CMD =$(which apt-get) \n " +
+            " get_package() \n " +
+            "{ \n PACKAGE = $1 \n" +
+            " if [[!-z $YUM_CMD]]; then \n" +
+            " yum install $PACKAGE - y \n" +
+            " elif[[!-z $APT_CMD]]; then \n" +
+            " apt install $PACKAGE - y \n" +
+            " elif[[!-z $APT_GET_CMD]]; then \n" +
+            " apt - get install $PACKAGE - y \n" +
+            " else \n" +
+            " echo \"Could not install $PACKAGE due to incompatible package manager\" \n" +
+            " exit 1; \n" +
+            " fi \n" +
+            " } \n" +
+            " get_package awscli \n" +
+            " (aws s3 cp s3://$BUCKET/$GUID/controller.sh controller.sh) \n \n" +
+            "(bash controller.sh)";
+
+
+    }
+}
