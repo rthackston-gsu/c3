98b7833a3f6c2b23d473f37ed14d7feb61523d4a
kshanki <35243588+kshanki@users.noreply.github.com>
2018-01-29 02:43:29 +0100

    Update myworker.sh

--- source/scripts/myworker.sh
+++ source/scripts/myworker.sh
@@ -1,37 +1,13 @@
+#cloud-boothook
 #!/bin/bash
 GUID=<GUID>
 BUCKET=<BUCKET>
-PATH_1="C:\users\task\"
-
-INSTANCE_ID=(curl http://169.254.169.254/latest/meta-data/)
-echo '$GUID'
-echo "$INSTANCE_ID"
-
-
 apt install awscli -y
-
 aws configure set aws_secret_access_key <secret_access_key>
 aws configure set region <region_name>
 aws configure set output json
 aws configure set aws_access_key_id <access_key_id>
-
-results(){
-aws s3 cp "C:\users\*.txt" s3://$BUCKET/$GUID/$INSTANCE_ID
-}
-
-For files in $PATH_1
-do
-aws s3 cp $files s3://$BUCKET/$GUID/
-
-done
-
-$TASK_PATH="s3://$BUCKET/$GUID/"
-
-For files in $TASK_PATH
-do
-echo "Performing Tasks"
-bash "$TASK_PATH"
-echo "Tasks Completed"
-done
-results()
-(shutdown -h now)
+Instance_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+bash $(aws s3 ls s3://$BUCKET/$GUID/task.sh)> /home/ec2-user/result2.txt
+aws s3 mv /home/ec2-user/result2.txt s3://$BUCKET/$GUID/$Instance_ID/
+shutdown -h now
