bcfea8295135dfb6a954e04a10ca83bab9b43366
kshanki <35243588+kshanki@users.noreply.github.com>
2018-02-13 10:11:35 -0500

    Update myworker.sh

--- source/scripts/myworker.sh
+++ source/scripts/myworker.sh
@@ -1,13 +1,18 @@
 #cloud-boothook
 #!/bin/bash
-GUID=<GUID>
-BUCKET=<BUCKET>
-apt install awscli -y
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py --user
+pip install awscli --user
+
+aws configure set aws_access_key_id <access_key_id>
 aws configure set aws_secret_access_key <secret_access_key>
 aws configure set region <region_name>
 aws configure set output json
-aws configure set aws_access_key_id <access_key_id>
+
+GUID=<GUID>
+BUCKET=<BUCKET>
 Instance_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-bash $(aws s3 ls s3://$BUCKET/$GUID/task.sh)> /home/ec2-user/result2.txt
-aws s3 mv /home/ec2-user/result2.txt s3://$BUCKET/$GUID/$Instance_ID/
+aws s3 cp s3://$BUCKET/$GUID/task.sh /home/ec2-user/
+bash /home/ec2-user/task.sh > /home/ec2-user/result.txt
+aws s3 mv /home/ec2-user/result.txt s3://$BUCKET/$GUID/$Instance_ID/
 shutdown -h now
