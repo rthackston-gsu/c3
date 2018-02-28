9e9375d6612f4100c9623489f0d0c2f57949b128
kshanki <35243588+kshanki@users.noreply.github.com>
2018-02-28 12:46:48 -0500

    Update myworker.sh

--- source/scripts/myworker.sh
+++ source/scripts/myworker.sh
@@ -13,6 +13,19 @@ GUID=<GUID>
 BUCKET=<BUCKET>
 Instance_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 aws s3 cp s3://$BUCKET/$GUID/task.sh /home/ec2-user/
-bash /home/ec2-user/task.sh > /home/ec2-user/result.txt
-aws s3 mv /home/ec2-user/result.txt s3://$BUCKET/$GUID/$Instance_ID/
+message=("{1:45}" "{2:55}" "{3:90}")
+for i in ${message[@]}
+do
+python send.py $i
+done
+
+count=1
+counter=${#message[@]}
+while [ $count -le $counter ]
+do
+MSG=$(python rec.py)
+bash /home/ec2-user/task.sh $MSG
+aws s3 mv result$MSG.txt s3://$BUCKET/$GUID/$Instance_ID/
+count=`expr $count + 1`
+done
 shutdown -h now
