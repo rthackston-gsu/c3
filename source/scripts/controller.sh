d4c7bc91183281696c823c4bc165b19a68496048
kshanki <35243588+kshanki@users.noreply.github.com>
2018-03-01 15:13:17 -0500

    Update controller.sh

--- source/scripts/controller.sh
+++ source/scripts/controller.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+source /home/ec2-user/magic.conf
+aws configure set aws_secret_access_key $MAGIC_SECRET_KEY
+aws configure set region $MAGIC_REGION_NAME
+aws configure set output $MAGIC_OUTPUT
+aws configure set aws_access_key_id $MAGIC_ACCESS_KEY
 
-aws configure set aws_secret_access_key <secret_access_key>
-aws configure set region <region_name>
-aws configure set output json
-aws configure set aws_access_key_id <access_key_id>
-
-(aws s3 cp s3://$BUCKET/$GUID/magic.conf /home/ec2-user/magic.conf)
-(aws s3 cp s3://$BUCKET/$GUID/worker.sh /home/ec2-user/worker.sh)
+#
+(aws s3 cp s3://$BUCKET/$GUID/magic.conf /home/ec2-user/)
+(aws s3 cp s3://$BUCKET/$GUID/worker.sh /home/ec2-user/)
 
 # Download and run Rabbitmq_server script to install and launch RabbitMQ
+(aws s3 cp s3://$BUCKET/$GUID/rabbitmq.sh /home/ec2-user/)
+bash /home/ec2-user/rabbitmq.sh
 
 # Download and run XYZ script to populate queue with jobs/messages
 message=("A" "B" "C")
@@ -16,6 +19,7 @@ for i in ${message[@]}
 do
 python send.py $i
 done
+
 # TODO: Read config values from magic.conf
 source /home/ec2-user/magic.conf
 (aws ec2 run-instances --image-id $MAGIC_IMAGE_ID --count $MAGIC_COUNT --instance-type $MAGIC_INSTANCE_TYPE --key-name $MAGIC_KEY_NAME --security-group-ids $MAGIC_SGI --user-data file://worker.sh)
