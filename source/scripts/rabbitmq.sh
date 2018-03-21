62a6f3dc7548dea219493d115f4e0c7393d86924
kshanki <35243588+kshanki@users.noreply.github.com>
2018-03-21 13:36:01 -0400

    Update rabbitmq.sh

--- source/scripts/rabbitmq.sh
+++ source/scripts/rabbitmq.sh
@@ -13,22 +13,22 @@ sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 sudo yum install -y erlang
 
 # Download the latest RabbitMQ package using wget:
-wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.2/rabbitmq-server-3.2.2-1.noarch.rpm
+wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.6.15/rabbitmq-server-3.6.15-1.el6.noarch.rpm
 
 # Add the necessary keys for verification:
 sudo rpm --import http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
 
 # Install the .RPM package using YUM:
-sudo yum install -y rabbitmq-server-3.2.2-1.noarch.rpm
+sudo yum install -y rabbitmq-server-3.6.15-1.el6.noarch.rpm
 
 # To have RabbitMQ start as a daemon by default
 sudo chkconfig rabbitmq-server on
 
 # To start the service:
-sudo /sbin/service rabbitmq-server start
+sudo service rabbitmq-server start
 
 #Enabling rabbitmq-management 
 sudo rabbitmq-plugins enable rabbitmq_management
 
 #Restarting the rabbitmq-server
-sudo /sbin/service rabbitmq-server restart
+sudo service rabbitmq-server restart
