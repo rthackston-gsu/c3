040041d23018520f14d266aa45a0208fc60dc3d1
rthackston-gsu <rthackston@georgiasouthern.edu>
2018-03-21 15:40:00 -0400

    Simplified user data script

--- source/scripts/userdata.sh
+++ source/scripts/userdata.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
-GUID=<GUID>
-BUCKET=<BUCKET>
 
-YUM_CMD=$(which yum)
-APT_CMD=$(which apt)
-APT_GET_CMD=$(which apt-get)
-
-get_package() {
-  PACKAGE = $1
-  if [[ ! -z $YUM_CMD ]]; then
-    yum install $PACKAGE -y
-  elif [[ ! -z $APT_CMD ]]; then
-    apt install $PACKAGE -y
-  elif [[ ! -z $APT_GET_CMD ]]; then
-    apt-get install $PACKAGE -y
-  else
-    echo "Could not install $PACKAGE due to incompatible package manager"
-    exit 1;
-  fi
-}
-
-get_package awscli
-aws s3 cp s3://$BUCKET/$GUID/controller.sh
-bash /home/ec2-user/controller.sh
+cd ~
+aws s3 cp s3://magic-beta/test/magic-beta.sh .
+chmod +x magic-beta.sh
+./magic-beta.sh
