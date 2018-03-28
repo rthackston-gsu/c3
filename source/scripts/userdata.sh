79356a264ad0159bcfe37b914cab6472ea0177bd
kshanki <35243588+kshanki@users.noreply.github.com>
2018-03-28 10:30:42 -0400

    Update userdata.sh

--- source/scripts/userdata.sh
+++ source/scripts/userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ~
-aws s3 cp s3://magic-beta/test/magic-beta.sh .
-chmod +x magic-beta.sh
-./magic-beta.sh
+aws s3 cp s3://magic-beta/test/controller.sh .
+chmod +x controller.sh
+./controller.sh
