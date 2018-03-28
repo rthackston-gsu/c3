6a23e26c86103294d1f8388ec309d83acb4bb360
kshanki <35243588+kshanki@users.noreply.github.com>
2018-03-28 10:26:39 -0400

    Update worker.sh

--- source/scripts/worker.sh
+++ source/scripts/worker.sh
@@ -2,24 +2,32 @@
 GUID=<GUID>
 BUCKET=<BUCKET>
 
-apt install awscli -y
-
-aws s3 cp s3://$BUCKET/$GUID/task.sh /home/ec2-user/
-aws s3 cp s3://$BUCKET/$GUID/rec.py /home/ec2-user/
+aws s3 cp s3://$BUCKET/$GUID/task.sh .
+aws s3 cp s3://$BUCKET/$GUID/rec.py .
 
 # Download and run Rabbitmq_server script to install and launch RabbitMQ
-(aws s3 cp s3://$BUCKET/$GUID/rabbitmq.sh /home/ec2-user/)
-bash /home/ec2-user/rabbitmq.sh
+aws s3 cp s3://$BUCKET/$GUID/rabbitmq.sh .
+./rabbitmq.sh
 
 # Start while loop
-count=1
-counter=${#message[@]}
-while [ $count -le $counter ]
+while true
 do
 MSG=$(python rec.py)
-bash /home/ec2-user/task.sh $MSG
+bash task.sh $MSG
 aws s3 mv result$MSG.txt s3://$BUCKET/$GUID/$Instance_ID/
-count=`expr $count + 1`
+if [[ $MSG == None ]]
+then
+break
+fi
 done
+shutdown -h now
+#count=1
+#counter=${#message[@]}
+#while [ $count -le $counter ]
+#do
+#MSG=$(python rec.py)
+#bash /home/ec2-user/task.sh $MSG
+#aws s3 mv result$MSG.txt s3://$BUCKET/$GUID/$Instance_ID/
+#count=`expr $count + 1`
+#done
 
-(shutdown -h now)
