4afb476effa1c02fe2e0944297c1b46913c476fe
kshanki <35243588+kshanki@users.noreply.github.com>
2018-03-28 10:29:22 -0400

    Update controller.sh

--- source/scripts/controller.sh
+++ source/scripts/controller.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-
 # copying the script to the ec2 home directory
-aws s3 cp s3://$BUCKET/$GUID/magic.conf /home/ec2-user/
-aws s3 cp s3://$BUCKET/$GUID/worker.sh /home/ec2-user/
-aws s3 cp s3://$BUCKET/$GUID/send.py /home/ec2-user/
+aws s3 cp s3://$BUCKET/$GUID/magic.conf .
+aws s3 cp s3://$BUCKET/$GUID/worker.sh .
+aws s3 cp s3://$BUCKET/$GUID/send.py .
 
 # Download and run Rabbitmq_server script to install and launch RabbitMQ
-aws s3 cp s3://$BUCKET/$GUID/rabbitmq.sh /home/ec2-user/
-bash /home/ec2-user/rabbitmq.sh
+aws s3 cp s3://$BUCKET/$GUID/rabbitmq.sh .
+chmod +x rabbitmq.sh
+./rabbitmq.sh
 
 # Download and run XYZ script to populate queue with jobs/messages
 message=("A" "B" "C")
@@ -18,6 +18,6 @@ python send.py $i
 done
 
 # TODO: Read config values from magic.conf
-source /home/ec2-user/magic.conf
-(aws ec2 run-instances --image-id $MAGIC_IMAGE_ID --iam-instance-profile Name="$MAGIC_IAM_USER" --count $MAGIC_COUNT --instance-type $MAGIC_INSTANCE_TYPE --key-name $MAGIC_KEY_NAME --security-group-ids $MAGIC_SGI --user-data file://worker.sh)
+source magic.conf
+aws ec2 run-instances --image-id $MAGIC_IMAGE_ID --iam-instance-profile Name="$MAGIC_IAM_USER" --count $MAGIC_COUNT --instance-type $MAGIC_INSTANCE_TYPE --key-name $MAGIC_KEY_NAME --security-group-ids $MAGIC_SGI --user-data file://worker.sh
 
