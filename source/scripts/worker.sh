9cd4d97d2320d1bed7045d1b4f136709b16c57ff
kshanki <35243588+kshanki@users.noreply.github.com>
2018-04-02 13:29:20 -0400

    Update worker.sh

--- source/scripts/worker.sh
+++ source/scripts/worker.sh
@@ -4,18 +4,22 @@ BUCKET=<BUCKET>
 
 Instance_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 aws s3 cp s3://$BUCKET/$GUID/controller_ip.sh .
+
 aws s3 cp s3://$BUCKET/$GUID/task.sh .
+chmod +x task.sh
 aws s3 cp s3://$BUCKET/$GUID/rec.py .
 
 # Download and run Rabbitmq_server script to install and launch RabbitMQ
 aws s3 cp s3://$BUCKET/$GUID/rabbitmq.sh .
+chmod +x rabbitmq.sh
 ./rabbitmq.sh
 
 # Start while loop
 while true
 do
-MSG=$(python rec.py $(cat controller_ip.txt))
-bash task.sh $MSG
+a=$(cat controller_ip.txt)
+MSG=$(python rec.py $a)
+./task.sh $MSG
 aws s3 mv result$MSG.txt s3://$BUCKET/$GUID/$Instance_ID/
 if [[ $MSG == None ]]
 then
