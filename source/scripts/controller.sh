51f987aacb008db2fc9593c7c83f04e9ebbd2755
kshanki <35243588+kshanki@users.noreply.github.com>
2018-04-04 09:20:23 -0400

    Update controller.sh

--- source/scripts/controller.sh
+++ source/scripts/controller.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+BUCKET=magicbucket123456789
+GUID=6ccbd3e0-d9b0-413a-a059-bcbcb4969644
 ip=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-echo $ip > ip.txt
-aws s3 cp ip.txt s3://$BUCKET/$GUID/
+echo $ip> controller_ip.txt
+aws s3 cp controller_ip.txt s3://$BUCKET/$GUID/
 
 # copying the script to the ec2 home directory
 aws s3 cp s3://$BUCKET/$GUID/magic.conf .
-aws s3 cp s3://$BUCKET/$GUID/worker.sh .
+aws s3 cp s3://$BUCKET/$GUID/magicworker.sh .
 aws s3 cp s3://$BUCKET/$GUID/send.py .
 
 # Download and run Rabbitmq_server script to install and launch RabbitMQ
@@ -21,7 +23,9 @@ do
 python send.py $i
 done
 
+# Setting AWS region
+aws configure set region us-east-2
+
 # TODO: Read config values from magic.conf
 source magic.conf
-aws ec2 run-instances --image-id $MAGIC_IMAGE_ID --iam-instance-profile Name="$MAGIC_IAM_USER" --count $MAGIC_COUNT --instance-type $MAGIC_INSTANCE_TYPE --key-name $MAGIC_KEY_NAME --security-group-ids $MAGIC_SGI --user-data file://worker.sh
-
+aws ec2 run-instances --image-id $MAGIC_IMAGE_ID --iam-instance-profile Name="$MAGIC_IAM_USER" --count $MAGIC_COUNT --instance-type $MAGIC_INSTANCE_TYPE --key-name $MAGIC_KEY_NAME --security-group-ids $MAGIC_SGI --user-data file://magicworker.sh
